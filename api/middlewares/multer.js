abe599920500155c6545e56b0465cca82836e232
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-02 17:04:41 +0900

    Aws config 경로 오류 수정

--- api/middlewares/multer.js
+++ api/middlewares/multer.js
@@ -1,7 +1,8 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const aws = require("aws-sdk");
-aws.config.loadFromPath(__dirname + "/../config/s3info.json");
+
+aws.config.loadFromPath(__dirname + "/../../config/s3info.json");
 
 const s3 = new aws.S3();
 const upload = multer({
