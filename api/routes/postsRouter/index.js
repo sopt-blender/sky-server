dae0a9d8b732fe94d50c267b77d28f2b53de393e
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-02 17:05:17 +0900

    포스트 CRUD 임시완료

--- api/routes/postsRouter/index.js
+++ api/routes/postsRouter/index.js
@@ -1,21 +1,18 @@
 import express from "express";
 import { postController } from "../../controllers/postController";
-import userModel from "../../models/userModel";
-import responseMessage from "../../modules/responseMessage";
-import statusCode from "../../modules/statusCode";
-import util from "../../modules/util";
+import upload from "../../middlewares/multer";
 const postsRouter = express.Router();
 
 // 전체 포스트
 postsRouter.get("/", postController.get_all);
 // 포스트 하나
-postsRouter.get("/:postId");
+postsRouter.get("/:postId", postController.get_post);
 // 포스트 업로드
-postsRouter.post("/");
+postsRouter.post("/", upload.single("image"), postController.create_post);
 // 포스트 수정
-postsRouter.put("/:postId");
+postsRouter.put("/:postId", postController.update_post);
 // 포스트 삭제
-postsRouter.delete("/:postId");
+postsRouter.delete("/:postId", postController.delete_post);
 // 포스트 찜
 postsRouter.post("/scrap/:postId");
 
