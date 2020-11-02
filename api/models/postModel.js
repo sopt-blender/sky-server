523e2feb0ef848852574c3a0407fbedb5b5797b5
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-02 16:08:48 +0900

    모델 타이포 수정

--- api/models/postModel.js
+++ api/models/postModel.js
@@ -11,4 +11,4 @@ const postSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
 });
 
-export default mongoos.model("post", postSchema);
+export default mongoose.model("Post", postSchema);
