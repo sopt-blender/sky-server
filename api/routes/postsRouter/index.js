18088d593157aef38bb919cfbbf259e64153a0cb
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-25 02:16:53 +0900

    풀 제대로 받기

--- api/routes/postsRouter/index.js
+++ api/routes/postsRouter/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { postController } from "../../controllers/postController";
-import {upload_desktop, upload_mobile} from "../../middlewares/multer";
+import { upload_desktop, upload_mobile } from "../../middlewares/multer";
 import auth from "../../middlewares/auth";
 
 const postsRouter = express.Router();
 
-/** 
+/**
  * 포스트 조회 (쿼리스트링 포함)
  * ?imagetype=1&page=1 : 데스크탑 post 1번째~20번째 조회
  * ?imagetype=2&page=2 : 모바일 post 21번째~40번째 조회
@@ -20,14 +20,32 @@ postsRouter.get("/", postController.get_my_posts);
 postsRouter.get("/:postId", postController.get_one_post);
 
 // 포스트 업로드 - 데스크탑
-postsRouter.post('/desktop', auth.checkToken, upload_desktop.single('image'), postController.create_one_post);
+postsRouter.post(
+  "/desktop",
+  auth.checkToken,
+  upload_desktop.single("image"),
+  postController.create_one_post,
+);
 // 포스트 업로드 - 모바일
-postsRouter.post('/mobile', auth.checkToken, upload_mobile.single('image'), postController.create_one_post);
+postsRouter.post(
+  "/mobile",
+  auth.checkToken,
+  upload_mobile.single("image"),
+  postController.create_one_post,
+);
 
 // 포스트 수정 - 데스크탑
-postsRouter.put("/desktop/:postId", upload_desktop.single("image"), postController.update_one_post);
+postsRouter.put(
+  "/desktop/:postId",
+  upload_desktop.single("image"),
+  postController.update_one_post,
+);
 // 포스트 수정 - 모바일
-postsRouter.put("/mobile/:postId", upload_mobile.single("image"), postController.update_one_post);
+postsRouter.put(
+  "/mobile/:postId",
+  upload_mobile.single("image"),
+  postController.update_one_post,
+);
 
 // 포스트 삭제
 postsRouter.delete("/:postId", postController.delete_one_post);
