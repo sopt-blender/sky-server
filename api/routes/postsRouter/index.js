e1adef21f35a2bc7248d8e25c3796fefcb0e3446
KangJunewoo <brianjune.dev@gmail.com>
2020-11-25 01:35:48 +0900

    [DOCS] 주석 추가

--- api/routes/postsRouter/index.js
+++ api/routes/postsRouter/index.js
@@ -3,8 +3,14 @@ import { postController } from "../../controllers/postController";
 import {upload_desktop, upload_mobile} from "../../middlewares/multer";
 const postsRouter = express.Router();
 
-// 전체 포스트 조회 : 쿼리스트링으로 imagetype=1 혹은 imagetype=2 필터 가능. 없을 경우 전체조회.
+/** 
+ * 포스트 조회 (쿼리스트링 포함)
+ * ?imagetype=1&page=1 : 데스크탑 post 1번째~20번째 조회
+ * ?imagetype=2&page=2 : 모바일 post 21번째~40번째 조회
+ * 쿼리스트링 없을경우 : 전체조회
+ */
 postsRouter.get("/", postController.get_all);
+
 // 포스트 하나 조회
 postsRouter.get("/:postId", postController.get_post);
 
