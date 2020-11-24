54b3b4f47e6a84face1dc4e1350944389f2ac639
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-25 03:35:34 +0900

    Revert to ES6+

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,21 +1,9 @@
-"use strict";
+import express from "express";
+import postsRouter from "./postsRouter";
+import usersRouter from "./usersRouter";
+const indexRouter = express.Router();
 
-exports.__esModule = true;
-exports.default = void 0;
+indexRouter.use("/users", usersRouter);
+indexRouter.use("/posts", postsRouter);
 
-var _express = _interopRequireDefault(require("express"));
-
-var _postsRouter = _interopRequireDefault(require("./postsRouter"));
-
-var _usersRouter = _interopRequireDefault(require("./usersRouter"));
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
-var indexRouter = _express.default.Router();
-
-indexRouter.use("/users", _usersRouter.default);
-indexRouter.use("/posts", _postsRouter.default);
-var _default = indexRouter;
-exports.default = _default;
+export default indexRouter;
--- app.js
+++ app.js
@@ -1,53 +1,38 @@
-"use strict";
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import statusCode from "./api/modules/statusCode";
+import indexRouter from "./api/routes";
+import cors from "cors";
 
-var _httpErrors = _interopRequireDefault(require("http-errors"));
+const app = express();
 
-var _express = _interopRequireDefault(require("express"));
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-var _path = _interopRequireDefault(require("path"));
+app.use("/api/v1", indexRouter);
 
-var _cookieParser = _interopRequireDefault(require("cookie-parser"));
-
-var _morgan = _interopRequireDefault(require("morgan"));
-
-var _statusCode = _interopRequireDefault(require("./api/modules/statusCode"));
-
-var _routes = _interopRequireDefault(require("./api/routes"));
-
-var _cors = _interopRequireDefault(require("cors"));
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
-var app = (0, _express.default)();
-app.use((0, _cors.default)());
-app.use((0, _morgan.default)("dev"));
-app.use(_express.default.json());
-app.use(
-  _express.default.urlencoded({
-    extended: false,
-  }),
-);
-app.use((0, _cookieParser.default)());
-app.use(_express.default.static(_path.default.join(__dirname, "public")));
-app.use("/api/v1", _routes.default); // catch 404 and forward to error handler
-
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next((0, _httpErrors.default)(404));
-}); // error handler
+  next(createError(404));
+});
+
+// error handler
 
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {}; // render the error page
-
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
   res.status(err.status || 500);
   console.log(err);
-  res.json({
-    error: {
-      message: err.message,
-    },
-  });
+  res.json({ error: { message: err.message } });
 });
+
 module.exports = app;
